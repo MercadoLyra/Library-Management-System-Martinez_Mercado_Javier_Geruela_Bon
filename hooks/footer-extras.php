bd7fe1c5afeb99c31aedc1928ee554c8c14a5a38
mayeeen <mayenjavier910@gmail.com>
2023-05-27 20:40:54 -0900

    Initial Commit

---
+++ hooks/footer-extras.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    Sorsogon State University Bulan Campus
+    </div>
+    <strong>Library Management System </strong> 
+  </footer>
